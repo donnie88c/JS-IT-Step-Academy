679eae41301d9396178218b12b2e7079960cd908
Doncho Ganev <cs_neeko@yahoo.com>
2025-03-28 19:29:28 +0200

    JS methods homework

---
+++ findTheBiggestNum.js
@@ -0,0 +1,5 @@
+let arr = [3,7,2,9,4];
+let result = Math.max(...arr);
+
+
+console.log(result);
---
+++ xNumber.js
@@ -0,0 +1,6 @@
+let arr = [2, 8, 4, 10, 3];
+let x = 5;
+let first = arr.find((num) => num > x);
+
+console.log(first);
+
